5fb2a7c2e7c9dde8f04b00c53b01f28bb9b25a8b
klynx <KevinLynx_L@hotmail.com>
2018-06-07 11:59:39 -0700

    Added callback response and changed host domain to request from

--- https_example.js
+++ https_example.js
@@ -3,13 +3,18 @@ var https = require("https");
 console.log("complete");
 
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  //console.log('Response ', response);
+response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  }); //convert the chunk to string
 }
 
 console.log("I'm about to make the request!");
@@ -17,3 +22,5 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
+
